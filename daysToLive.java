67ed29f079d60b93d8e0351e224b46e400b40e33
Aniruddh Kedlaya <akbazuka@Kedlayas-MacBook-Pro.local>
2020-01-19 13:00:48 -1000

    bye file added and hello file edited

---
+++ daysToLive.java
@@ -0,0 +1,9 @@
+public class daysToLive{
+
+	public static void main(String[]args){
+
+		int randomInt = (int)(10.0*Math.random());	
+	
+		System.out.println("You have "+randomInt+" days to live.");
+	}
+}
--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 	public static void main(String[] args){
 
 		//System.out.println("hello");
-		greeting("hello");
+		greeting("Aloha World!");
 	}
 
 	public static void greeting(String msg){
