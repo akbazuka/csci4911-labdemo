78a5d8c176635be762bf6a754ba6c301a7640a87
Aniruddh Kedlaya <akbazuka@Kedlayas-MacBook-Pro.local>
2020-01-19 12:26:47 -1000

    helper.java file added

---
+++ helper.java
@@ -0,0 +1,7 @@
+public class helper{
+
+	public static void main(String[]args){
+
+		System.out.println("Use \"touch\" and \"nano\" commands to create and edit a new file from mac terminal.");
+	}
+}
