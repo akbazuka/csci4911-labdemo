48fe4683c3e0592f21a03c9dcec012dbd44d9cdd
Aniruddh Kedlaya <akbazuka@Kedlayas-MacBook-Pro.local>
2020-01-19 12:15:03 -1000

    Greeting method added

--- hello.java
+++ hello.java
@@ -2,6 +2,12 @@ public class hello{
 
 	public static void main(String[] args){
 
-		System.out.println("hello");
+		//System.out.println("hello");
+		greeting("hello");
+	}
+
+	public static void greeting(String msg){
+
+		System.out.println(msg);
 	}
 }
